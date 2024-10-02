858566a603a621f07397796ca137705cbd2b990c
jskassa24 <jkassa@mail.pima.edu>
2024-10-01 17:35:09 -0700

    Update cis129_lab02_shortStory.py
    first CIS 129 lab

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,8 @@
 print('Hello World!')
+print('Once upon a time there was a duck.')
+print('The duck wanted grapes.')
+print('So the duck went to a lemonade stand.')
+print('The duck did not get grapes.')
+#Author: Julai Kassa
+#My first Python lab in CIS 129
+#prints a simple story
